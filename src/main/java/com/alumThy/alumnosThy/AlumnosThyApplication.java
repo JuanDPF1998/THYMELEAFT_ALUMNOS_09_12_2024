420e7dde00d46df7ea2a098c1647826ba4999c56
JuanDPF <juandanielperezfalcon1998qgmail.com>
2024-12-09 12:49:41 -0600

    nuevo proyecto cascaron 1 fase

---
+++ src/main/java/com/alumThy/alumnosThy/AlumnosThyApplication.java
@@ -0,0 +1,13 @@
+package com.alumThy.alumnosThy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlumnosThyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlumnosThyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alumnosThymeleaft/alumnosThy/AlumnosThyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alumnosThymeleaft.alumnosThy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlumnosThyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
