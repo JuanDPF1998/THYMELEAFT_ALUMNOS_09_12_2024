03a32d8018a55c1d42c8281e9f85e5a654fa4412
JuanDPF <juandanielperezfalcon1998qgmail.com>
2024-12-10 11:12:31 -0600

    servicio e impl 3 fase accion listar

---
+++ src/main/java/com/alumThy/alumnosThy/service/alumnosService.java
@@ -0,0 +1,10 @@
+package com.alumThy.alumnosThy.service;
+
+import java.util.List;
+
+import com.alumThy.alumnosThy.model.alumnosModel;
+
+public interface alumnosService {
+
+	public List<alumnosModel> listarTodos();
+}
---
+++ src/main/java/com/alumThy/alumnosThy/service/alumnosServiceImpl.java
@@ -0,0 +1,30 @@
+package com.alumThy.alumnosThy.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.alumThy.alumnosThy.model.alumnosModel;
+import com.alumThy.alumnosThy.reposity.alumnosRepositorio;
+
+public class alumnosServiceImpl implements alumnosService{
+
+	/*
+	 * SE UTILIZA PARA INYECTAR AUTOMATICAMENTE DEPENDENCIAS EN LOS COMPONENTES GESTIONADOS
+	 * POR EL CONTENEDOR DE SPRING, COMO SERVICIOS, CONTROLADORES, REPOSITORIOS.
+	 */
+	@Autowired
+	private alumnosRepositorio repositorio;
+	
+	/*
+	 * METODO LISTAR TODOS, ESTE METODO SE ENCARGA DE BUSCAR TODOS LOS REGISTROS
+	 * DE LA BASE DE DATOS, REGRESA UNA RESPUESTA ACCEDIENDO AL OBJETO REPOSITORIO DE LA
+	 * INTERFAZ ALUMNOSREPOSITORIO QUE HEREDA DE JPAREPO ACCEDIENDO A TODOS LOS METODOS
+	 * EN ESTE CASO FINALL (BUSCA TODOS).
+	 */
+	@Override
+	public List<alumnosModel> listarTodos() {
+		return repositorio.findAll();
+	}
+
+}
